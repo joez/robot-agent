bcd3edf63daf065b553281548bb4aaa824d91d9a
Joe Zheng <joe.zheng.cn@gmail.com>
2016-06-15 15:27:12 +0800

    fine tune speed

--- app/controllers/photo.js
+++ app/controllers/photo.js
@@ -20,13 +20,14 @@ module.exports = function (ros) {
           ros : ros,
           name : '/camera/rgb/image_raw/compressed',
           messageType : 'sensor_msgs/CompressedImage',
-          throttle_rate : 1
+          throttle_rate : 1000,
+          queue_length : 2
         });
 
         topic.subscribe(function(message) {
           debug("captured a photo with format: " + message.format);
           cb({ type : "image/jpeg", data : message.data});
-          topic.unsubscribe();
+            topic.unsubscribe();
         });
       }
 
--- app/controllers/teleop.js
+++ app/controllers/teleop.js
@@ -8,6 +8,18 @@ module.exports = function (ros) {
     handle : function (cmd, data) {
       var handled = true;
 
+      var speed = 1;
+
+      var getLinearSpeed = function(speed) {
+        var base = 0.1; // 0.1 m/s
+        return Math.min(2, Math.max(0.1, speed * base));
+      }
+
+      var getAngularSpeed = function(speed) {
+        var base = 15;  // 15 degrees/s
+        return Math.min(180, Math.max(10, speed * base)) * Math.PI / 180;
+      }
+
       // linear x and y movement and angular z movement
       var x = 0;
       var y = 0;
@@ -15,16 +27,16 @@ module.exports = function (ros) {
 
       switch (cmd) {
         case 'left':
-          z = 1;
+          z = getAngularSpeed(speed);
           break;
         case 'right':
-          z = -1;
+          z = -getAngularSpeed(speed);
           break;
         case 'up':
-          x = 0.5;
+          x = getLinearSpeed(speed);
           break;
         case 'down':
-          x = -0.5;
+          x = -getLinearSpeed(speed);
           break;
         case 'stop':
           break;
@@ -33,12 +45,14 @@ module.exports = function (ros) {
       }
 
       if (handled) {
-        debug('cmd: ' + cmd);
+        debug('cmd: ' + cmd + ': ' + x + ', ' + y + ', ' + z);
 
         var cmdVel = new roslib.Topic({
           ros : ros,
           name : '/cmd_vel_mux/input/navi',
-          messageType : 'geometry_msgs/Twist'
+          messageType : 'geometry_msgs/Twist',
+          throttle_rate : 500,
+          queue_length : 10
         });
 
         var twist = new roslib.Message({
